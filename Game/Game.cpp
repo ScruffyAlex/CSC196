8b554b4e79566a5282516989e7e9ca876c0b8983
Alex McDowell <scruffyalexhs@gmail.com>
2020-07-09 11:44:28 -0600

    math funcitons

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -3,6 +3,7 @@
 #include "Color.h"
 #include "Math/Random.h"
 //#include "Math/Vector2.h"
+#include "Math/Math.h"
 #include "Color.h"
 #include "Math/Transform.h"
 #include "Graphics/Shape.h"
@@ -11,20 +12,20 @@
 
 
 nc::Shape ship;
-
-const size_t NUM_POINTS = 40;
-float speed = 300.0f;
-
-//std::vector<nc::Vector2> points = { { 0, -3 }, { 3, 3 }, { 0, 1 }, { -3, 3 }, { 0, -3 } };
-
 nc::Transform transform{ {400,300},4,0 };
+
+DWORD prevTime;
+DWORD deltaTime;
+
+
+float t{ 0 };
+
+float speed = 200.0f;
 
 float frametime;
 float roundTime{ 0 };
 bool gameOver{ false };
 
-DWORD prevTime;
-DWORD deltaTime;
 
 
 bool Update(float dt)// delta time (60 fps) (1/60 = 0.016)
@@ -33,6 +34,8 @@ bool Update(float dt)// delta time (60 fps) (1/60 = 0.016)
 	DWORD time = GetTickCount();
 	deltaTime = time - prevTime; // current time frame - previous time frame
 	prevTime = time;
+
+	t = t + dt*5;
 
 	frametime = dt;
 	roundTime += dt;
@@ -50,33 +53,40 @@ bool Update(float dt)// delta time (60 fps) (1/60 = 0.016)
 	Core::Input::GetMousePos(x, y);
 
 
-
-	nc::Vector2 force;
+	nc::Vector2 force{ 0,0 };
 	if (Core::Input::IsPressed('W')) { force = nc::Vector2::forward * speed * dt; }
 	nc::Vector2 direction = force;
 	direction = nc::Vector2::Rotate(direction, transform.angle);
 	transform.position = transform.position + direction;
 
-	if (Core::Input::IsPressed('A')) { transform.angle = transform.angle - (dt * 3.0f); }
-	if (Core::Input::IsPressed('D')) { transform.angle = transform.angle + (dt * 3.0f); }
+	//rotate
+	if (Core::Input::IsPressed('A')) { transform.angle = transform.angle - (nc::DegreesToRadians(360.0f)*dt); }
+	if (Core::Input::IsPressed('D')) { transform.angle = transform.angle + (nc::DegreesToRadians(360.0f)*dt); }
+
+	//keep within borders
+	transform.position = nc::Clamp(transform.position, { 0,0 }, { 800,600 });
 
 	return quit;
 }
 
 void Draw(Core::Graphics& graphics)
-{
-	
-	
+{	
 	graphics.DrawString(10, 10, std::to_string(frametime).c_str());
 	graphics.DrawString(10, 20, std::to_string(1.0f/frametime).c_str());
 	graphics.DrawString(10, 30, std::to_string(deltaTime/1000.0f).c_str());
+
+	float v = (std::sin(t) + 1.0f); // -1 <-> 1 | 0 - 2
+
+	nc::Color c = nc::Lerp(nc::Color{0,0,1}, nc::Color{1,0,0},v);
+	graphics.SetColor(c);
+	
+	nc::Vector2 p = nc::Lerp(nc::Vector2{400,300}, nc::Vector2{100,100},v);
+	graphics.DrawString(p.x,p.y,"Pew Pew Boi");
 
 	if (gameOver) graphics.DrawString(400, 300, "Game Over");
 
 	ship.Draw(graphics, transform);
 	
-	
-
 }
 
 int main()
